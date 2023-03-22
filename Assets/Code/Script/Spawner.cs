8cba8ee507d4c244ab744ab33b56038c5f954556
Marianne Hernholm <marhernh@stud.ntnu.no>
2023-03-22 16:16:06 +0100

    Added collission and rigidbody to fruit

---
+++ Assets/Code/Script/Spawner.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject[] fruits;
+    public GameObject peanutBomb;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRandomGameObject());
+    }
+
+    IEnumerator SpawnRandomGameObject()
+    {
+        yield return new WaitForSeconds(Random.Range(1, 2));
+        int randomFruitIndex = Random.Range(0, fruits.Length);
+
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-10,11), 5, Random.Range(-10,11));
+
+        if (Random.value <= .6f)
+        {
+            Instantiate(fruits[randomFruitIndex], randomSpawnPosition, Quaternion.identity);
+        }
+        
+        else
+        {
+            Instantiate(peanutBomb, randomSpawnPosition, Quaternion.identity);
+        }
+        
+        StartCoroutine(SpawnRandomGameObject());
+    }
+
+    
+}
