266da8c78d355b0a91649aaf860321d4e8298449
Marianne Hernholm <marhernh@stud.ntnu.no>
2023-04-20 14:08:51 +0200

    Removed some comments

--- Assets/Code/Script/PlaceInBasket.cs
+++ Assets/Code/Script/PlaceInBasket.cs
@@ -14,28 +14,9 @@ public class PlaceInBasket : MonoBehaviour
         }
     }
 
-    /*void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("FRUIT collides with: " + other.gameObject.tag);
-        
-        if (stopOnCollision.caughtByLeft || stopOnCollision.caughtByRight)
-        {
-            Debug.Log("Fruit has been caught. Check if its triggered by the Basket.");
-            
-            if (other.gameObject.tag == "FruitBasket")
-            {   
-                Debug.Log("FRUIT HIT BASKET. ADD POINT MOVEMENT 2");
-                Feedback.instance.AddPointMovement_2();     // Add point for movement 2
-                Debug.Log("DESTROY THIS FRUIT");
-                Destroy(this.gameObject);
-            }
-        }
-    }*/
-
     void OnTriggerEnter(Collider other)
-    {
-        //Debug.Log("FRUIT collides with: " + other.gameObject.tag);
-        
+    {   
+        // Check if fruit has been caught AND if it has hit the Basket   
         if ((stopOnCollision.caughtByLeft || stopOnCollision.caughtByRight) && (other.gameObject.tag == "FruitBasket"))
         {
             Debug.Log("FRUIT HIT BASKET. ADD POINT MOVEMENT 2");
