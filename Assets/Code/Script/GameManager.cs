d4a9750a3ac191a60dfe9fbb7183c1745aae506f
Marianne Hernholm <marhernh@stud.ntnu.no>
2023-04-21 15:00:49 +0200

    Started implementing a main menu using Unity UI Toolkit and Unity Builder

--- Assets/Code/Script/GameManager.cs
+++ Assets/Code/Script/GameManager.cs
@@ -4,17 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private static GameManager instance;    // Only one version of this variable and no copies can be made of it
-    public static GameManager Instance
-    {
-        get
-        {
-            if (instance == null)
-            {
-                // do something
-            }
-        }
-    }
+    //private static GameManager instance;    // Only one version of this variable and no copies can be made of it
+    /*public static GameManager Instance;
+    
 
     public bool _isGameOver;
     
@@ -27,7 +19,19 @@ public class GameManager : MonoBehaviour
 
     void Awake()
     {
+        // Set instance to this, and do a nullcheck
+        if (Instance == null)   // if there is no instance already
+        {
+            Debug.LogError("Game Manager is Null!");
+            Instance = this;
+        }
+        else if (Instance != this)  // if there is already an instance and its not this instance
+        {
+            // Instance = this;
+            Destroy(gameObject);
+        }
 
+        
     }
 
 
@@ -40,6 +44,6 @@ public class GameManager : MonoBehaviour
     public bool IsGameOver()
     {
         return _isGameOver;
-    }
+    }*/
 
 }
