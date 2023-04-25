6b1c2c2c473165a3abeff6e3bd6c10c8dbcf88fd
Marianne Hernholm <marhernh@stud.ntnu.no>
2023-04-26 00:08:48 +0200

    Updated QuitManager

--- Assets/Code/Script/QuitManager.cs
+++ Assets/Code/Script/QuitManager.cs
@@ -7,6 +7,9 @@ public class QuitManager : MonoBehaviour
     public void QuitGame()
     {
         Debug.Log("QUIT GAME");
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
         Application.Quit();
     }
 }
