22387fee1c50ccde32bd4aa6412e40756074f355
Marianne Hernholm <marhernh@stud.ntnu.no>
2023-04-27 16:51:58 +0200

    Edited spawner script to spawn total Repetitions times

--- Assets/Code/Script/Spawner.cs
+++ Assets/Code/Script/Spawner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Spawns fruit objects at random locations
@@ -11,7 +12,6 @@ using TMPro;
 public class Spawner : MonoBehaviour
 {
     public GameObject[] fruitsPrefab;
-    public GameObject peanutPrefab;
     [HideInInspector] public StopOnCollision stopOnCollision;
     private float delayTime = 0;
     private int totalRepetitions;
@@ -19,7 +19,7 @@ public class Spawner : MonoBehaviour
     
     void Awake()
     {
-        // use to stop spawner when a fruit is caught
+        // Use to stop spawner when a fruit is caught
         stopOnCollision = GetComponent<StopOnCollision>();
 
         // Get the difficulty settings from DifficultyManager
@@ -29,32 +29,59 @@ public class Spawner : MonoBehaviour
     
     void Start()
     {
-        Debug.Log("SPAWN DELAY TIME: " + delayTime);
-        Debug.Log("SPAWN REPETITIONS: " + totalRepetitions);
+        Debug.Log("Spawn delay time: " + delayTime);
+        Debug.Log("Spawn repetitions: " + totalRepetitions);
+        
         StartCoroutine(SpawnRandomGameObject());
     }
 
     IEnumerator SpawnRandomGameObject()
     {
-        yield return new WaitForSeconds(Random.Range(1, 2));
-        int randomFruitIndex = Random.Range(0, fruitsPrefab.Length);
-        //Vector3 randomSpawnPosition = new Vector3(Random.Range(-1,2), 4, Random.Range(-1,2));
-        Vector3 randomSpawnPosition = new Vector3(0, 4, 0);                                                        // Force position of spawns to always hit player (for testing)
-
-        
-        if (Random.value <= .6f)
+        for (int i = 0; i < totalRepetitions; i++)
         {
+            // Wait for a set time
+            // yield return new WaitForSeconds(Random.Range(1, 2));
+            yield return new WaitForSeconds(delayTime);
+
+            // Pick a fruit randomly   
+            int randomFruitIndex = Random.Range(0, fruitsPrefab.Length);
+
+            // Pick a random position for the fruit to spawn 
+            // Vector3 randomSpawnPosition = new Vector3(Random.Range(-1,2), 4, Random.Range(-1,2));
+            Vector3 randomSpawnPosition = new Vector3(0, 4, 0);    // Force position of spawns to always hit player (for testing)
+
+            //spawn a fruit prefab
             Instantiate(fruitsPrefab[randomFruitIndex], randomSpawnPosition, Quaternion.identity);
-        }
-        
-        else
-        {
-            Instantiate(peanutPrefab, randomSpawnPosition, Quaternion.identity);
+            Debug.Log("# spawns: " + i);
         }
 
-        yield return new WaitForSeconds(delayTime); // Wait time between each spawn
-        StartCoroutine(SpawnRandomGameObject());
+        // End game by loading the credit scene
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene("Credits"); 
+    }  
+
+    /*
+    I start: 
+    for (int i = 0; i < totalRepetitions; i++)
+    {
+        Debug.Log("# spawns: " + i);
     }
+    
+    IEnumerator SpawnRandomGameObject()
+    {
+        // Wait for a set time
+        // yield return new WaitForSeconds(Random.Range(1, 2));
+        yield return new WaitForSeconds(delayTime);
 
+        // Pick a fruit randomly   
+        int randomFruitIndex = Random.Range(0, fruitsPrefab.Length);
+
+        // Pick a random position for the fruit to spawn 
+        // Vector3 randomSpawnPosition = new Vector3(Random.Range(-1,2), 4, Random.Range(-1,2));
+        Vector3 randomSpawnPosition = new Vector3(0, 4, 0);  // Force position of spawns to always hit player (for testing)
+
+        //spawn a fruit prefab
+        Instantiate(fruitsPrefab[randomFruitIndex], randomSpawnPosition, Quaternion.identity);
+    } */  
 }
 
